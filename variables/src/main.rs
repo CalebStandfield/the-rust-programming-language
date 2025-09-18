54ba7430770be665d3737a971d9a425052b4287d
CalebJStandfield <54382087+CalebStandfield@users.noreply.github.com>
2025-09-18 11:23:04 -0600

    Added the files from my old repo to this new one since git makes me sad and wasn't working :(

---
+++ variables/src/main.rs
@@ -0,0 +1,96 @@
+use std::io;
+
+fn main() {
+    let mut y = 5;
+    println!("The value of x is: {y}");
+    y = 6;
+    println!("The value of x is: {y}");
+
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+
+    // addition
+    let _sum = 5 + 10;
+
+    // subtraction
+    let _difference = 95.5 - 4.3;
+
+    // multiplication
+    let _product = 4 * 30;
+
+    // division
+    let _quotient = 56.7 / 32.2;
+    let _truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let _remainder = 43 % 5;
+
+    let _t = true;
+
+    let _f: bool = false; // with explicit type annotation
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let _five_hundred = x.0;
+
+    let _six_point_four = x.1;
+
+    let _one = x.2;
+
+    let _a = [1, 2, 3, 4, 5];
+
+    let _months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    let _c = 'z';
+    let _z: char = 'ℤ'; // with explicit type annotation
+    let _heart_eyed_cat = '😻';
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (_x, y, _z) = tup;
+
+    println!("The value of y is: {y}");
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    if index < a.len() {
+        let element = a[index];
+        println!("The value of the element at index {index} is: {element}");
+    } else {
+        println!("The desired index is out of range");
+    }
+}
