46d61f84781bdaac8f9ff678cb2dca370fb25662
CalebJStandfield <54382087+CalebStandfield@users.noreply.github.com>
2025-10-19 22:54:43 -0600

    completeted chapter 5 - structs
    I think this will be useful for defining what a food object is, and what an excersize is for the fitnesswithfriendsapp

---
+++ structs_examples/src/main.rs
@@ -0,0 +1,55 @@
+fn main() {
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+
+    // Refactored with tuple
+    let rectangle = (30, 50);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area2(rectangle)
+    );
+
+    // Refactored with struct
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area3(&rect1)
+    );
+
+    // println!("rect1 is {rect1}");
+    println!("rect1 is {rect1:?}");
+
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect1);
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area2(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area3(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
