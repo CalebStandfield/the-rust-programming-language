93871ac931b8e0ebb80cc9dafc73395a7e01601e
CalebJStandfield <54382087+CalebStandfield@users.noreply.github.com>
2025-10-24 19:26:10 -0600

    Chapter 10
    When to panic and when not to panic, that is the question of course. I think itll take a little bit of trial and error when it comes to panicing, and using Result<T, E> definitly a lot to unpack there. but with the FWF app I think Im going to get about as much practice as possible implementing database queries and https requests

---
+++ recoverable_errors/src/main.rs
@@ -0,0 +1,46 @@
+use std::fs::File;
+use std::io::{self, ErrorKind, Read};
+
+fn main() {
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {error:?}");
+            })
+        } else {
+            panic!("Problem opening the file: {error:?}");
+        }
+    });
+}
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let username_file_result = File::open("hello.txt");
+//
+//     let mut username_file = match username_file_result {
+//         Ok(file) => file,
+//         Err(e) => return Err(e),
+//     };
+//
+//     let mut username = String::new();
+//
+//     match username_file.read_to_string(&mut username) {
+//         Ok(_) => Ok(username),
+//         Err(e) => Err(e),
+//     }
+// }
+
+// better way to write it with the ? operator
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+// The shortest version
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     fs::read_to_string("hello.txt")
+// }
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
