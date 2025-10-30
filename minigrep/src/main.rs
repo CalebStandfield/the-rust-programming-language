3e4d7f66ada83773c9c4738316ec9dda31434a50
CalebJStandfield <54382087+CalebStandfield@users.noreply.github.com>
2025-10-29 18:32:17 -0600

    chapter 13.2,3,4
    I tried so hard to get search_case_insensitive to be able to call search, as to not rewrite similar looking code in both functions, but each time Id run into issues with returning temporaries, since .to_lowercase() creates new temorary strings and then those would be dropped at the end of the function which woulld dangle the references. So a good learning experience and a frustraighting one at that

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,12 @@
+use minigrep::{search, search_case_insensitive};
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
-use std::error::Error;
-use minigrep::{search, search_case_insensitive};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -44,12 +42,19 @@ struct Config {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
